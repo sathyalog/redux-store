915f9b9ed37dff6f49ff6231dafe656af0656921
Sathya <sathya.vakacharla@gmail.com>
2020-01-29 15:07:04 +0530

    created store and added store to the provider

---
+++ src/store.js
@@ -0,0 +1,14 @@
+import {createStore, compose, applyMiddleware} from 'redux';
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = () => ({
+    'message': 'Hello! Welcome to Redux Store'
+});
+
+const store = createStore(
+    rootReducer,
+    composeEnhancer(applyMiddleware()),
+);
+
+export default store;
