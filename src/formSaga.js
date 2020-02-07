53e1c8c9254de29d687dba61eef865437000eb67
Sathya <sathya.vakacharla@gmail.com>
2020-02-07 18:41:11 +0530

    mock json updated

--- src/formSaga.js
+++ src/formSaga.js
@@ -2,7 +2,7 @@ import { put, takeLatest, all } from 'redux-saga/effects';
 import * as types from './constants';
 
 function* fetchFormData() {
-    const data = yield fetch('https://firebasestorage.googleapis.com/v0/b/redux-store-c6c24.appspot.com/o/mockForm.json?alt=media&token=bd9afcc0-6859-44c5-9a2f-124b54c8e5e1')
+    const data = yield fetch('https://firebasestorage.googleapis.com/v0/b/redux-store-c6c24.appspot.com/o/mockForm.json?alt=media&token=7c98bc90-c245-4fa6-821a-2695a11453d6')
         .then(response => response.json() );      
     yield put({ type: types.GET_FORMDATA_RECEIVED, data: data });
 }
